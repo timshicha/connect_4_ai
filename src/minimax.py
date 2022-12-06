94c5843a42a4c3102024b3f5440dd591fb0323cd
Timofey Shichalin <shichatim@gmail.com>
2022-12-05 22:02:30 -0800

    minimax fixes

--- src/minimax.py
+++ src/minimax.py
@@ -39,7 +39,7 @@ class Minimax_Agent:
                 max_value = max(max_value, self.min_move(board, max_value, depth_left - 1))
             board.unmove() # Undo the simulation move
             # If we can prune:
-            if(max_value >= kickout_value):
+            if(max_value > kickout_value):
                 return max_value
         return max_value
 
@@ -71,16 +71,26 @@ class Minimax_Agent:
                 min_value = min(min_value, self.max_move(board, min_value, depth_left - 1))
             board.unmove() # Undo the simulation move
             # If we can prune:
-            if(min_value <= kickout_value):
+            if(min_value < kickout_value):
                 return min_value
         return min_value
-            
-        
-    
+
+
+
     # Get the best move for player given a board.
     def get_best_move(self, board, search_depth):
+        # Return the move that's closer to the middle. This will help
+        # settle ties, since middle moves are generally prefered.
+        def better_move(move1, move2):
+            if(move1 == None): return move2
+            order = [3,2,4,1,5,0,7]
+            for move in order:
+                if(move1 == move): return move1
+                if(move2 == move): return move2
+            return move1
+
         max_value = -inf
-        best_move = None
+        best_move = None # Ties will be settled by choosing middle-most column.
         legal_moves = board.get_legal_moves()
         
         # Try each move:
@@ -88,7 +98,7 @@ class Minimax_Agent:
             turn = board.get_turn()
             board.move(move) # Make the move
             # If this was a winning move, just return (guaranteed best value).
-            # If full, this must be only move, so also okay to return.
+            # If full, this must be the only move, so also okay to return.
             if(board.check_win(turn) or board.check_full()):
                 board.unmove()
                 return move
@@ -100,13 +110,22 @@ class Minimax_Agent:
                 if(estimate > max_value):
                     max_value = estimate
                     best_move = move
+                # If equally good best move found, favor middle move
+                elif(estimate == max_value):
+                    max_value = estimate
+                    best_move = better_move(best_move, move)
             # Otherwise play out:
             else:
                 estimate = self.min_move(board, max_value, search_depth - 1)
+                print(f"Move {move} estimate: {estimate}")
                 # If new best move found
                 if(estimate > max_value):
                     max_value = estimate
                     best_move = move
+                # If equally good best move found, favor middle move
+                elif(estimate == max_value):
+                    max_value = estimate
+                    best_move = better_move(best_move, move)
             board.unmove() # Undo the simulation move
         return best_move
         
